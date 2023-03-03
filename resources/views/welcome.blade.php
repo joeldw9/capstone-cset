4a252c0051c3d75f664963319dd14ce8388872ec
joeldw9 <joeldw9@gmail.com>
2023-03-03 13:40:58 -0500

    made css work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Computer Repair Shop</title>
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ url('main.css') }}" rel="stylesheet" type="text/css" >
   </head>
   <body>
     <header>
