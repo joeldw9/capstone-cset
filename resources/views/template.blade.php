ab57b45326d7b4cf67d8cbaa1ad7c12a157cd6c6
Paul-1106 <93672435+Paul-1106@users.noreply.github.com>
2023-03-06 15:53:02 -0500

    login not finished

---
+++ resources/views/template.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <link href="{{ url('template.css') }}" rel="stylesheet" type="text/css" >
+    </head>
+    <body>
+        <header class="topSection">
+        </header>
+        <section class="section1">
+        </section>
+        <footer class="bottomSection">
+        </footer>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/template', function() {
+    return view('template');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+?>
