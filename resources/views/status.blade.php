ba414df691dd6828ea00796dd2dc26eb9284e2ca
Paul-1106 <93672435+Paul-1106@users.noreply.github.com>
2023-03-27 21:20:33 -0400

    status updated again

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -14,6 +14,17 @@
                 <img class="image2" src="images/yellowtruck.png">
                 <img class="image3" src="images/greencheck.png">
             </div>
+            <div class="stepManage">
+                <span class="circle1"></span>
+                <span class="circle2"></span>
+                <span class="circle3"></span>
+            </div>
+            <div class="line1">
+                <span class="regLine1"></span>
+            </div>
+            <div class=statsNow>
+                <h1 class="orderNow">Your order tracking status: </h1>
+            </div>
         </section>
         <footer class="bottomSection">
         </footer>
