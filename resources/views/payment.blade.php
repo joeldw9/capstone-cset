ca81321c8b1cebc3e6d82464cba6db9e0178bfa8
Paul-1106 <93672435+Paul-1106@users.noreply.github.com>
2023-04-05 15:18:13 -0400

    payment model updated

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -19,22 +19,22 @@
                 <form class='form1'>
                     <h1 class="cardTitle">Card</h1><br><br>
                     <h3>Username: <h3 style="color: red; position: relative;"> <?php echo $_SESSION["username"]; ?></h3></h3><br><br>
-                    <input type="radio" name="card" value="Credit">Credit
-                    <input type="radio" name="card" value="Debit">Debit<br><br><br>
+                    <input type="radio" name="card" value="Credit" required>Credit
+                    <input type="radio" name="card" value="Debit" required>Debit<br><br><br>
                     <label for="cardNumber">Card Number: </label>
-                    <input type="text" name="cardNum"><br><br>
-                    <label for="expireDate">Expiration Date: </label>
-                    <input type="date" name="expDate"><br><br>
+                    <input type="text" name="cardNum" required><br><br>
+                    <label for="expireDate">Expiration Year: </label>
+                    <input type="number" name="expDate" min="1000" max="9999" required><br><br>
                     <label for="cvc"> CVC: </label>
-                    <input type="number" name="cvc"><br><br>
+                    <input type="number" name="cvc" min="100" max="999" required><br><br>
                     <label for="zip"> Zip Code: </label>
-                    <input type="number" name="zipCode"><br><br><br>
-                    <input class="cardSubmit" type="submit" value="Continue to Checkout" name="submit">
+                    <input type="number" name="zipCode" required><br><br><br>
+                    <input class="cardSubmit" type="submit" value="Submit payment" name="submit">
                 </form>
                 <form class="form2">
                     <h1 class="paypalTitle">Paypal</h1><br><br>
                     <h3>Username: <h3 style="color: red; position: relative;"> <?php echo $_SESSION["username"]; ?></h3></h3><br><br>
-                    <input class="paypalSubmit" type="submit" value="Continue to Checkout" name="submit">
+                    <input class="paypalSubmit" type="submit" value="Submit payment" name="submit">
                 </form>
             </div>
         </section>
